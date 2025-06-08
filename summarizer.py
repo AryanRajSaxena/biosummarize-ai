1283cfffa3c4c7d07925ad7a1f9d359fb4a05271
AryanRajSaxena <aryanraj052002@gmail.com>
2025-06-08 14:30:33 +0530

    Switch to Pegasus summarizer for Streamlit compatibility

--- summarizer.py
+++ summarizer.py
@@ -1,13 +1,12 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 
 class Summarizer:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("microsoft/BioGPT")
-        self.model = AutoModelForCausalLM.from_pretrained("microsoft/BioGPT")
+        self.tokenizer = AutoTokenizer.from_pretrained("google/pegasus-xsum")
+        self.model = AutoModelForSeq2SeqLM.from_pretrained("google/pegasus-xsum")
 
-    def summarize(self, text, max_tokens=150):
-        prompt = "Summarize: " + text
-        inputs = self.tokenizer(prompt, return_tensors="pt", truncation=True).to(self.model.device)
+    def summarize(self, text, max_tokens=200):
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True, max_length=1024)
         outputs = self.model.generate(**inputs, max_new_tokens=max_tokens)
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
